d8d607c0cf6e8aa966c5e3dd439a9c483275ef75
晨宇 苏 <894645804@qq.com>
2017-11-29 20:21:25 +0800

    MainUI

---
+++ WebApplication1/Controllers/MainUIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MainUIController : Controller
+    {
+        // GET: MainUI
+        public ActionResult StudentUI()
+        {
+            return View();
+        }
+        // GET: MainUI
+        public ActionResult TeacherUI1()
+        {
+            return View();
+        }
+        public ActionResult TeacherUI2()
+        {
+            return View();
+        }
+        public ActionResult ModifyStuInfo()
+        {
+            return View();
+        }
+        public ActionResult ModifyTeaInfo()
+        {
+            return View();
+        }
+
+
+
+    }
+}
