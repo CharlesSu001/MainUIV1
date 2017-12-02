cd6bef457baa8f2d109bb3eb295405400ee540ba
晨宇 苏 <894645804@qq.com>
2017-12-02 20:11:32 +0800

    controller初步完成

--- WebApplication1/Controllers/MainUIController.cs
+++ WebApplication1/Controllers/MainUIController.cs
@@ -31,9 +31,13 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public JsonResult SendData()
+        public JsonResult SendTeacherData()
         {
-            return Json( new {username="邱明",teacherid="2432012522" });
+            return Json( new {username="邱明",teacherid="2432012522",course1= "J2EE", course2 = "OOAD",course3 = "JAVA",course4 = "OS" ,phone= "18159275806",school="厦门大学" });
+        }
+        public JsonResult SendStudentData()
+        {
+            return Json(new { username = "韦小宝", stuid = "243201522xxxxx", course1 = "J2EE", course2 = "OOAD", course3 = "JAVA", course4 = "OS", teacher1 = "邱明", teacher2 = "邱明", teacher3 = "王美红", teacher4 = "吴清强", phone = "18159275806", school = "厦门大学" });
         }
 
     }
--- WebApplication1/Scripts/studentinfo.js
+++ WebApplication1/Scripts/studentinfo.js
@@ -1,26 +1,33 @@
 ﻿$(function () {
     $.ajax({
         type: "post",
-        url: "/mainui/sendteacherdata",
+        url: "/mainui/sendstudentdata",
         dataType: "json",
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         success: function (data) {
-            console.log(data);
+            console.log(data);//将返回的值打印出来看看，然后你就知道怎么用了，可能如data.username
             var name = data.username;
-            var teacherid = data.teacherid;
+            var stuid = data.stuid;
             var course1 = data.course1;
             var course2 = data.course2;
             var course3 = data.course3;
             var course4 = data.course4;
+            var teacher1 = data.teacher1;
+            var teacher2 = data.teacher2;
+            var teacher3 = data.teacher3;
+            var teacher4 = data.teacher4;
             var phone = data.phone;
             var school = data.school;
-
             $("#username").text(name);
-            $("#teacherid").text(teacherid);
+            $("#stuid").text(stuid);
             $("#course1").text(course1);
             $("#course2").text(course2);
             $("#course3").text(course3);
             $("#course4").text(course4);
+            $("#teacher1").text(teacher1);
+            $("#teacher2").text(teacher2);
+            $("#teacher3").text(teacher3);
+            $("#teacher4").text(teacher4);
             $("#phone").text(phone);
             $("#school").text(school);
         },
